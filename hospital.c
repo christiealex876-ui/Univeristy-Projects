96c14dc04eba0fa36a657f69167f2dc8d8b9dba2
Alex C. Christie <christiealex876@gmail.com>
2026-08-27 22:50:18 -0500

    Finished Code

--- hospital.c
+++ hospital.c
@@ -22,6 +22,8 @@ d. Return both patient number and day of highest reading.
 #include <time.h>
 
 void average(int hospital[10][7]);
+void above_100(int hospital[10][7]);
+void highest(int hospital[10][7]);
 
 int main()
 {
@@ -49,25 +51,78 @@ int main()
         printf("\n");
     }
 
+    average(hospital);
+    above_100(hospital);
+    highest(hospital);
+
     return 0;
 }
 
 void average(int hospital[10][7])
 {
-    for(int count = 0; count < 10; count++)
+
+    printf("\n--- Patient Averages ---\n");
+
+    for(int patient = 0; patient < 10; patient++)
     {
         float sum = 0;
 
-        for(int count = 0; count < 7; count++)
+        for(int days = 0; days < 7; days++)
         {
-            sum += hospital[count][count];
+            sum += hospital[patient][days];
         }
 
-        float average = sum / 7.0;
+        printf("Patient %d: %.2f bpm\n", patient + 1, sum / 7.0);
+    }
+
+}
+
+
+void above_100(int hospital[10][7])
+{
+    int over = 0;
+
+    printf("\n---Patients With Readings Above 100 bpm---\n");
+
+    for(int patient = 0; patient < 10; patient++)
+    {
+        over = 0;
+
+        for(int days = 0; days < 7; days++)
+        {
+            if(hospital[patient][days] > 100)
+            {
+                over = 1;
+            }
+        }
+
+        if(over == 1)
+        {
+            printf("Patient %d has a heartbeat over 100\n", patient + 1);
+        }
     }
 }
 
-void average(int hospital[10][7])
+void highest(int hospital[10][7])
 {
-    
+    int high = 59, highest_patient = 0, highest_day = 0;
+
+    for(int patient = 0; patient < 10; patient++)
+    {
+        for(int days = 0; days < 7; days++)
+        {
+            if(hospital[patient][days] > high)
+            {
+                high = hospital[patient][days];
+                highest_day = days;
+                highest_patient = patient;
+            }
+
+        }
+    }
+
+    printf("\n---Highest Heart BPM---\n");
+    printf("Highest heart bpm: \t%d\n", high);
+    printf("Highest Patient \t%d\n", highest_patient + 1);
+    printf("Highest Day: \t\t%d\n", highest_day + 1);
 }
