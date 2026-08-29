bf00f43474c45f7964267eec1c29f5938723cfca
Alex C. Christie <christiealex876@gmail.com>
2026-08-29 13:02:47 -0500

    Initial Commit

---
+++ Book_ID_Storage.c
@@ -0,0 +1,57 @@
+/*
+Author: Alex C. Christie
+Date: 08/29/2026
+Description: Do
+
+A small community library stores book IDs in random order. The librarian wants them
+sorted numerically before printing labels. Write a C program that accepts 10 book ID
+numbers. Sorts them in ascending order using Bubble Sort. Displays the sorted list.
+Counts and displays the number of swaps performed.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int library(int id[], int size);
+int sort(int id[], int size);
+
+int main()
+{
+    int id[10];
+
+    library(id, 10);
+
+
+    system("pause");
+    return 0;
+}
+
+int library(int id[], int size)
+{
+    for(int count = 0; count < size; count++)
+    {
+        printf("ID %d: ", count + 1);
+        scanf("%f", &id[count]);
+    }
+}
+
+int sort(int id[], int size)
+{
+    int swap_count = 0, temp = 0;
+
+    for(int count = 0; count < size; count++)
+    {
+        for(int i = 0; size - i - 2; i++)
+        {
+            if(id[i] > id[i + 1])
+            {
+                temp = id[i];
+                id[i] = id[i + 1];
+                temp = id[i + 1];
+                
+                swap_count++;
+            }
+        }
+    }
+}
